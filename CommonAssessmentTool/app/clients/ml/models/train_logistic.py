329cc662d778ff0faa9b2ec62340c56d46527ffa
yukki1998 <xu.lian@northeastern.edu>
2025-04-09 01:19:24 -0700

    Fix import order using isort

--- CommonAssessmentTool/app/clients/ml/models/train_logistic.py
+++ CommonAssessmentTool/app/clients/ml/models/train_logistic.py
@@ -1,4 +1,5 @@
 import pickle
+
 import pandas as pd
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
--- CommonAssessmentTool/app/models 2.py
+++ CommonAssessmentTool/app/models 2.py
@@ -3,18 +3,13 @@ Database models module defining SQLAlchemy ORM models for the Common Assessment 
 Contains the Client model for storing client information in the database.
 """
 
-from app.database import Base
-from sqlalchemy import (
-    Column,
-    Integer,
-    String,
-    Boolean,
-    ForeignKey,
-    CheckConstraint,
-    Enum,
-)
-from sqlalchemy.orm import relationship
 import enum
+
+from sqlalchemy import (Boolean, CheckConstraint, Column, Enum, ForeignKey,
+                        Integer, String)
+from sqlalchemy.orm import relationship
+
+from app.database import Base
 
 
 class UserRole(str, enum.Enum):
