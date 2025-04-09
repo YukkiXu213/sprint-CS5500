8925b4e1a1e24218f1c6d4bef89aa8ac7902b504
yukki1998 <xu.lian@northeastern.edu>
2025-04-09 10:25:15 -0700

    Modify conftest.py

--- CommonAssessmentTool/tests/conftest.py
+++ CommonAssessmentTool/tests/conftest.py
@@ -2,10 +2,10 @@ import pytest
 from fastapi.testclient import TestClient
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from ..app.database import Base, get_db
-from ..app.main import app
-from ..app.auth.router import get_password_hash
-from ..app.models import User, UserRole, Client, ClientCase
+from app.database import Base, get_db
+from app.main import app
+from app.auth.router import get_password_hash
+from app.models import User, UserRole, Client, ClientCase
 
 # Create test database
 SQLALCHEMY_DATABASE_URL = "sqlite:///./test.db"
